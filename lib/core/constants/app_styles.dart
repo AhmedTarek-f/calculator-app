f1dc0ee85c2b24327ee355c7e5d0c66bc21bb132
AhmedTarek <o0ahmedtarek0o@gmail.com>
2025-04-02 12:17:26 +0200

    adding app styles

---
+++ lib/core/constants/app_styles.dart
@@ -0,0 +1,15 @@
+import 'package:calculator_app/core/constants/app_colors.dart';
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+abstract class AppStyles{
+  static TextStyle fontSize48Medium = GoogleFonts.poppins(
+    fontSize: 48,
+    fontWeight: FontWeight.w500,
+    color: AppColors.white
+  );
+  static TextStyle fontSize24Normal = GoogleFonts.poppins(
+      fontSize: 24,
+      fontWeight: FontWeight.w400,
+  );
+}
