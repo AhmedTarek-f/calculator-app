d9438b35d46bac5d847ab2839a6363e7a0518cdc
AhmedTarek <o0ahmedtarek0o@gmail.com>
2025-04-02 15:04:14 +0200

    adding app icons class

---
+++ lib/core/constants/app_icons.dart
@@ -0,0 +1,5 @@
+abstract class AppIcons{
+  static const String deleteIcon = "assets/icons/delete_icon.svg";
+  static const String divisionIcon = "assets/icons/division_icon.svg";
+  static const String multiplicationIcon = "assets/icons/multiplication_icon.svg";
+}
