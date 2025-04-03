c0e0571133c5e8fb979fc26d9399d37abcce3e20
AhmedTarek <o0ahmedtarek0o@gmail.com>
2025-04-03 20:25:51 +0200

    adding logic to calculator button item

--- lib/features/calculator_home/presentation/views/widgets/calculator_button_item.dart
+++ lib/features/calculator_home/presentation/views/widgets/calculator_button_item.dart
@@ -2,7 +2,9 @@ import 'package:calculator_app/core/constants/app_colors.dart';
 import 'package:calculator_app/core/constants/app_styles.dart';
 import 'package:calculator_app/core/utils/screen_size.dart';
 import 'package:calculator_app/features/calculator_home/data/models/button_model.dart';
+import 'package:calculator_app/features/calculator_home/presentation/views_model/calculator_cubit.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 
 class CalculatorButtonItem extends StatelessWidget {
@@ -15,25 +17,40 @@ class CalculatorButtonItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      alignment: Alignment.center,
-      width: buttonData.width ?? ScreenSize.getWidth(context)*0.1653,
-      height: buttonData.height ?? ScreenSize.getHeight(context)*0.07635,
-      decoration: BoxDecoration(
-        color: buttonData.buttonBackgroundColor?? AppColors.gray,
-        borderRadius: BorderRadius.circular(ScreenSize.getWidth(context)*0.0426),
-      ),
-      child: !buttonData.buttonLabel.contains(".svg")?
-      Padding(
-        padding: buttonData.buttonLabel == "." ? EdgeInsets.only(bottom: ScreenSize.getHeight(context)*0.014): EdgeInsets.zero,
-        child: Text(
-          buttonData.buttonLabel,
-          style: buttonData.buttonLabelStyle??AppStyles.fontSize32Medium,
+    final controller = BlocProvider.of<CalculatorCubit>(context);
+    return InkWell(
+      onTap: (){
+        controller.changeCalculatorValue(value: buttonData.buttonLabel);
+      },
+      highlightColor: AppColors.lightBlue,
+      borderRadius: BorderRadius.circular(ScreenSize.getWidth(context)*0.04),
+      child: Container(
+        alignment: Alignment.center,
+        width: buttonData.width ?? ScreenSize.getWidth(context)*0.1653,
+        height: buttonData.height ?? ScreenSize.getHeight(context)*0.07635,
+        decoration: BoxDecoration(
+          color: buttonData.buttonBackgroundColor?? AppColors.gray,
+          borderRadius: BorderRadius.circular(ScreenSize.getWidth(context)*0.0426),
+          boxShadow: [
+            BoxShadow(
+              color: buttonData.buttonBackgroundColor?.withValues(alpha: 0.6) ?? AppColors.gray.withValues(alpha: 0.6),
+              blurStyle: BlurStyle.outer,
+              blurRadius: 5
+            )
+          ]
         ),
-      ):
-      SvgPicture.asset(
-        buttonData.buttonLabel,
-        fit: BoxFit.scaleDown,
+        child: !buttonData.buttonLabel.contains(".svg")?
+        Padding(
+          padding: buttonData.buttonLabel == "." ? EdgeInsets.only(bottom: ScreenSize.getHeight(context)*0.014): EdgeInsets.zero,
+          child: Text(
+            buttonData.buttonLabel,
+            style: buttonData.buttonLabelStyle??AppStyles.fontSize32Medium,
+          ),
+        ):
+        SvgPicture.asset(
+          buttonData.buttonLabel,
+          fit: BoxFit.scaleDown,
+        ),
       ),
     );
   }
