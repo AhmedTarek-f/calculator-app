aedd0cf00485ddf49e2c3d7ba40814855f98aa7c
AhmedTarek <o0ahmedtarek0o@gmail.com>
2025-04-03 20:26:53 +0200

    adding bloc provider

--- lib/features/calculator_home/presentation/views/calculator_home_view.dart
+++ lib/features/calculator_home/presentation/views/calculator_home_view.dart
@@ -1,13 +1,20 @@
 import 'package:calculator_app/features/calculator_home/presentation/views/widgets/calculator_home_view_body.dart';
+import 'package:calculator_app/features/calculator_home/presentation/views_model/calculator_cubit.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class CalculatorHomeView extends StatelessWidget {
   const CalculatorHomeView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: CalculatorHomeViewBody(),
+    return  Scaffold(
+      body: BlocProvider(
+        create: (context) => CalculatorCubit(),
+        child: const SafeArea(
+            child: CalculatorHomeViewBody()
+        ),
+      ),
     );
   }
 }
