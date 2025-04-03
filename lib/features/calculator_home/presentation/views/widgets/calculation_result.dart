f5bc054eb8238a9e39183accec20fa53a246274e
AhmedTarek <o0ahmedtarek0o@gmail.com>
2025-04-03 20:24:31 +0200

    adding logic to calculation result

--- lib/features/calculator_home/presentation/views/widgets/calculation_result.dart
+++ lib/features/calculator_home/presentation/views/widgets/calculation_result.dart
@@ -1,6 +1,9 @@
 import 'package:calculator_app/core/constants/app_styles.dart';
 import 'package:calculator_app/core/utils/screen_size.dart';
+import 'package:calculator_app/features/calculator_home/presentation/views_model/calculator_cubit.dart';
+import 'package:calculator_app/features/calculator_home/presentation/views_model/calculator_state.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class CalculationResult extends StatelessWidget {
   const CalculationResult({
@@ -9,17 +12,40 @@ class CalculationResult extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final controller = BlocProvider.of<CalculatorCubit>(context);
     return SizedBox(
-      height: ScreenSize.getHeight(context)*0.418,
+      height: ScreenSize.getHeight(context) * 0.4,
       width: ScreenSize.getWidth(context),
       child: Padding(
-        padding: EdgeInsets.symmetric(horizontal: ScreenSize.getWidth(context)*0.048),
-        child: Align(
-          alignment: Alignment.centerRight,
-          child: Text(
-            "=12,454",
-            style: AppStyles.fontSize48Medium,
-          ),
+        padding: EdgeInsets.symmetric(
+            horizontal: ScreenSize.getWidth(context) * 0.048),
+        child: BlocBuilder<CalculatorCubit, CalculatorState>(
+          builder: (context, state) {
+            return Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.end,
+              children: [
+                if(controller.beforeOperationValue != null) FittedBox(
+                  child: Text(
+                    controller.beforeOperationValue!,
+                    style: AppStyles.fontSize32Normal,
+                  ),
+                ),
+                if(controller.beforeOperationValue != null) SizedBox(height: ScreenSize.getHeight(context)*0.006,),
+                if(controller.beforeOperationValue != null) Text(
+                controller.currentOperator!,
+                style: AppStyles.fontSize24Normal,
+                ),
+                if(controller.beforeOperationValue != null) SizedBox(height: ScreenSize.getHeight(context)*0.006,),
+                FittedBox(
+                  child: Text(
+                    state.calculationValue,
+                    style: AppStyles.fontSize48Medium,
+                  ),
+                ),
+              ],
+            );
+          },
         ),
       ),
     );
